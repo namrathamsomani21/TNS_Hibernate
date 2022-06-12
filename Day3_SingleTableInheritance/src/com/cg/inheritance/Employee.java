8a7c0d663f5b64ae705e8a81596be9a7f1dd47a4
Namratha <namrathamsomani21@gmail.com>
2022-06-12 21:49:58 +0530

    First commit

---
+++ Day3_SingleTableInheritance/src/com/cg/inheritance/Employee.java
@@ -0,0 +1,50 @@
+package com.cg.inheritance;
+
+import java.io.Serializable;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="emp_type",discriminatorType=DiscriminatorType.STRING)
+@DiscriminatorValue("EMP")
+//Discriminator is used to store the details of the child class
+
+public class Employee implements Serializable {
+private static final long serialVersionUID = 1L;
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)//to create the series of ids automatically
+private int id;
+private String name;
+private double salary;
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public double getSalary() {
+	return salary;
+}
+public void setSalary(double salary) {
+	this.salary = salary;
+}
+
+}
